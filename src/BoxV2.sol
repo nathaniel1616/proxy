76de3740632ec594e9d5416bb69e4fcd6776e3c8
Nathaniel Yeboah <yeboahnat99@gmail.com>
2024-06-04 00:21:23 +0000

    chore: Add OpenZeppelin contracts as submodules and remappings

---
+++ src/BoxV2.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+// import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 {
+    uint256 internal number;
+
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    //get Version
+
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+
+    // function _authorizeUpgrade(address newImplementation) internal override {}
+}
